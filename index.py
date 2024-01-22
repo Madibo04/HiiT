848514e6b15a05de7b06157cf3de2fcaf4c8e2cd
Madibo04 <153165475+Madibo04@users.noreply.github.com>
2024-01-22 15:01:06 +0100

    Update index.py

--- index.py
+++ index.py
@@ -16,6 +16,6 @@ def roman_to_int(s):
     return result
 
 # Example usage:
-roman_numeral = "XX"
+roman_numeral = "XI"
 integer_value = roman_to_int(roman_numeral)
 print(f"The integer value of {roman_numeral} is {integer_value}")
